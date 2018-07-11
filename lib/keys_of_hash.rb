7839da7c4bb4fd2baff30d43a13e6f5607d9d9ed
Andrew Och <andrewoch95@gmail.com>
2018-07-11 16:09:39 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    key_array = []
+    hash.each do |key, value|
+      if args.include?(value)
+        key_array.push(key)
+      end
+    end
   end
 end
